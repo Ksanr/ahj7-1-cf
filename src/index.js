9900ddc527bbf2554ec5356ff1eed21b69f5ec8c
Aleksandr Lipunov <alipunov@mail.ru>
2026-05-20 16:36:19 +0000

    add CORS for all

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default {
     const corsHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers': '*', //'Content-Type, Authorization',
       'Access-Control-Max-Age': '86400',
     };
 
